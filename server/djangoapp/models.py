3603880d055742de31dbd3947a42d74738227146
parsae80 <67233772+parsae80@users.noreply.github.com>
2022-08-19 00:28:24 +0430

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,9 +25,24 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    CAR_TYPES = [
+        ("Sedan", "sedan"),
+        ("SUV","SUV"),
+        ("WAGON", "Wagon"),
+    ]
+    carmake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 50)
+    dealerId = models.CharField(max_length = 50)
+    carType = models.CharField(max_length = 50, choices=CAR_TYPES)
+    year = models.DateField()
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
+# class CarDealer(models.Model):
+     
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
